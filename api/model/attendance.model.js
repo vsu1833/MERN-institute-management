cecdf3d613fc8a508fe52e7fb1cfac69c7d26eac
viraj <vsu1833@gmail.com>
2025-02-11 23:31:41 +0530

    Your message

--- api/model/attendance.model.js
+++ api/model/attendance.model.js
@@ -4,7 +4,8 @@ const attendanceSchema = new mongoose.Schema({
   school:{type:mongoose.Schema.ObjectId, ref:'School'},
   student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
   class:{type:mongoose.Schema.Types.ObjectId, ref:"Class"},
-  date: { type: Date, required: true },
+  // hi there
+  date:{ type: Date, required: true },
   status: { type: String, enum: ['Present', 'Absent'], default: 'Absent' }
 }, { timestamps: true });
 
